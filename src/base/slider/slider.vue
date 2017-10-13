aaf97ea620a8280f98285a4b04cacc72b57d0e0e
yany2 <yuan.yan@dianrong.com>
2017-10-13 13:01:13 +0800

    slider

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="slider">
+    <div class="slier-group">
+      <slot>
+      </slot>
+    </div>
+    <div class="dots">
+      <span class="dot">
+
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'slider',
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="recommend" ref="recommend">
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          slider
+        </div>
+      </div>
+    </scroll>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
