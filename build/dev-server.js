3a199539717f17239ec8e0b63ad9d3f33c11674d
YanYuan <1025138284@qq.com>
2017-12-03 22:09:32 +0800

    lyric api

--- build/dev-server.js
+++ build/dev-server.js
@@ -26,6 +26,31 @@ const app = express()
 
 var apiRoutes = express.Router()
 
+apiRoutes.get('/lyric', function(req, res) {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  console.log(req.query)
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var matches = ret.match(reg)
+      if(matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((err) => {
+    console.log(err)
+  })
+})
+
 apiRoutes.get('/getDiscList', function(req, res) {
   var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
 
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -297,6 +297,7 @@
         }
         this.$nextTick(() => {
           this.$refs.audio.play()
+          this.currentSong.getLyric()
         })
       },
       playing(newPlaying) {
