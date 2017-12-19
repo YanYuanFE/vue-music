add113d81abb45d7e4e75c6a730e9452bfcc9800
yany2 <yuan.yan@dianrong.com>
2017-12-19 09:36:50 +0800

    rank

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Recommend from '@/components/recommend/recommend'
 import Singer from '@/components/singer/singer'
+import Rank from '@/components/rank/rank'
 import SingerDetail from '@/components/singer-detail/singer-detail'
+import TopList from '@/components/top-list/top-list'
 
 Vue.use(Router)
 
@@ -23,6 +25,16 @@ export default new Router({
         {
           path: ':id',
           component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/rank',
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
         }
       ]
     }
